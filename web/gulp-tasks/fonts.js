24e2230b180a195b24c512f82baa3d316f20ad74
Sunil Kumar <sunil.divyam@gmail.com>
2016-05-31 15:15:12 +0530

    Gulp | gulp-js-build renamed to js-build and all gulp custom tasks are moved to tasks subfolder existing deleted

--- web/gulp-tasks/fonts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(gulp, config) {
-	gulp.task('fonts', function() {
-		gulp.src(config.fontsDir + '/**/*')
-		.pipe(gulp.dest(config.targetDir + '/' + config.fontsDir));
-	});
-};
--- web/gulp-tasks/images.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(gulp, config) {
-	gulp.task('images', function() {
-		gulp.src(config.imagesDir + '/**/*')
-		.pipe(gulp.dest(config.targetDir + '/' + config.imagesDir));
-	});
-};
