1585df32021883b3bc5dff565bd2672d71583fec
Sunil Kumar <sunil.divyam@gmail.com>
2016-06-05 13:13:18 +0530

    RAIWEB | Recursive HTML template implementation for Nav Dynamic States Creation in core module

--- web/app/js/core/controllers/appController.js
+++ web/app/js/core/controllers/appController.js
@@ -11,63 +11,6 @@
         setCurrentBreakpoint();
 
         /*
-        *   assigns pageTitleService and metaInformationService to $rootScope
-        *   so that both are available in the Head section of HTML page
-        */
-
-        $rootScope.pageTitleService = pageTitleService;
-        $rootScope.metaInformationService = metaInformationService;
-
-        // fetches the Application Header information
-        appHeaderService.getAppHeaderInfo().then(function(data) {
-            setAppHeader(data);
-            setMetaInformation({
-                keywords: data.application.keywords,
-                description: data.application.description,
-                title: data.application.shortTitle
-            });
-        }, function() {
-            setAppHeader(false);    // resets the Header Information
-        });
-
-        /*
-        *   setAppHeader is a private method
-        *   It takes an Object param, and sets the various Header Information
-        *   If param of anyother type is passed, it resets all the Header Information to null.
-        *   So This method can be used to reset Header Information by passing false valueto param
-        */
-        function setAppHeader(headerInfo) {
-            if (headerInfo instanceof Object) {
-                $scope.appHeader = {
-                    logo: headerInfo.logo || null,
-                    navs: headerInfo.navs || null
-                };
-            } else {
-                $scope.appHeader = {
-                    logo: null,
-                    navs: null
-                };
-            }
-        }
-
-        /*
-        *   setMetaInformation is a private method
-        *   It takes an Object param, and sets the following meta Information of Page:
-        *   keywords, description and page Title
-        */
-        function setMetaInformation(metaInfo) {
-            if (metaInfo instanceof Object) {
-                metaInformationService.reset();
-                metaInformationService.appendMetaKeywords(metaInfo.keywords);
-                metaInformationService.setMetaDescription(metaInfo.description);
-                pageTitleService.setPageTitle(metaInfo.title);
-            } else {
-                metaInformationService.reset();
-                pageTitleService.setPageTitle();
-            }
-        }
-
-        /*
         *   setCurrentBreakpoint is a private method
         *   sets the current bootstrap breakpoint, on load or resize of the window.
         *   The responsiveDetectionService gives the current breakpoint based on the current window size
--- web/app/js/core/core.js
+++ web/app/js/core/core.js
@@ -1,105 +1,171 @@
 'use strict';
 (function() {
-	/**
-	*  	raiweb.core Module
-	* 	Description
-	*	This module is the base module for the application and has States configurations
-	*	and other core level configurations if any
-	*/
+    /**
+    *   raiweb.core Module
+    *   Description
+    *   This module is the base module for the application and has States configurations
+    *   and other core level configurations if any
+    */
 
-	/* 	This is $stateProviderRef variable and is used to create All States Dynamically
-	*	from appHeaderService fetched nav data
-	*/
-	var $stateProviderRef;
+    /*  This is $stateProviderRef variable and is used to create All States Dynamically
+    *   from appHeaderService fetched nav data
+    */
+    var $stateProviderRef;
 
-	angular.module('raiweb.core', [])
-	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-		$stateProviderRef = $stateProvider;
-		// Any invalid Url will redirect to "/home" url
-		$urlRouterProvider.otherwise('/home');
+    angular.module('raiweb.core', [])
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProviderRef = $stateProvider;
+        // Any invalid Url will redirect to "/home" url
+        $urlRouterProvider.otherwise('/home');
 
-		// Enables html5Mode Urls
-		$locationProvider.html5Mode({
-			enabled: false,
-			requireBase: false
-		});
+        // Enables html5Mode Urls
+        $locationProvider.html5Mode({
+            enabled: false,
+            requireBase: false
+        });
 
 
-	}])
-	/*
-	*	Description
-	*	Run method of core module, makes the $state and $stateParams Service, available
-	*	to the $rootScope Service
-	*/
-	.run(['$state', '$stateParams', '$rootScope', 'appHeaderService', function($state, $stateParams, $rootScope, appHeaderService) {
-		$rootScope.$state = $state;
-		$rootScope.$stateParams = $stateParams;
+    }])
+    /*
+    *   Description
+    *   Run method of core module, makes the $state and $stateParams Service, available
+    *   to the $rootScope Service
+    */
+    .run(['$state', '$stateParams', '$rootScope', 'appHeaderService','pageTitleService', 'metaInformationService',
+        function($state, $stateParams, $rootScope, appHeaderService, pageTitleService, metaInformationService) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
 
-		/*
-		*	Description
-		*	All States configuration using $stateProviderRef, a reference to the $stateProvider service.
-		*	All templates are loaded using $templateCache Service
-		*	All States configurations are loaded dynamically using appHeaderService ($http) in JSON format
-		*/
+        /*
+        *   assigns pageTitleService and metaInformationService to $rootScope
+        *   so that both are available in the Head section of HTML page
+        */
 
-		appHeaderService.getAppHeaderInfo().then(function(data) {
-			var navs = data && data.navs;
-			createStates(navs);
-		}, function() {
+        $rootScope.pageTitleService = pageTitleService;
+        $rootScope.metaInformationService = metaInformationService;
 
-		});
+        /*
+        *   Description
+        *   All States configuration using $stateProviderRef, a reference to the $stateProvider service.
+        *   All templates are loaded using $templateCache Service
+        *   All States configurations are loaded dynamically using appHeaderService ($http) in JSON format
+        */
 
-		/*
-		*	createStates is a private method
-		*	Description:
-		*	createStates method is a recursive method, which recursively iterates through the navs tree and
-		*	creates the corresponding states.
-		*/
-		function createStates(navs, parentStateName) {
-			if (!(navs instanceof Array)) {
-				return;
-			}
+        appHeaderService.getAppHeaderInfo().then(function(data) {
+            if (data instanceof Object) {
+                setAppHeader(data);
+                // creates states and child states, as well as adds full stateNames to Navs
+                createStates($rootScope.appHeader.navs);
+                if (data.application instanceof Object) {
+                    setMetaInformation({
+                        keywords: data.application.keywords,
+                        description: data.application.description,
+                        title: data.application.shortTitle
+                    });
+                } else {
+                     setMetaInformation(); //resets meta information
+                }
+            } else {
+                setMetaInformation();
+                setAppHeader(false);
+            }
+        }, function() {
+            setMetaInformation();
+            setAppHeader(false);
+        });
 
-			navs.filter(function(navItem) {
-				var stateName = parentStateName ? parentStateName + '.' + navItem.id : navItem.id;
-				var state = {
-					name: stateName,
-					url: '/' + navItem.id,
-					templateProvider: ['$templateCache', function($templateCache) {
-						return $templateCache.get(navItem.templateUrl);
-					}]
-				};
+        /*
+        *   createStates is a private method
+        *   Description:
+        *   createStates method is a recursive method, which recursively iterates through the navs tree and
+        *   creates the corresponding states.
+        */
+        function createStates(navs, parentStateName) {
+            if (!(navs instanceof Array)) {
+                return;
+            }
 
-				if (navItem.controllerName) {
-					state.controller = navItem.controllerName;
-				}
+            navs.filter(function(navItem) {
+                var stateName = parentStateName ? parentStateName + '.' + navItem.id : navItem.id;
+                /*  pushes full stateName into the navItem of the navs Array.
+                *   So that this can be available to header template
+                */
+                navItem.stateName = stateName;
+                var state = {
+                    name: stateName,
+                    url: '/' + navItem.id,
+                    templateProvider: ['$templateCache', function($templateCache) {
+                        return $templateCache.get(navItem.templateUrl);
+                    }]
+                };
 
-				$stateProviderRef.state(state);
-				createStates(navItem.navs, stateName);
-			});
-		}
-	}]);
+                if (navItem.controllerName) {
+                    state.controller = navItem.controllerName;
+                }
 
-	/*
-	*	Description
-	*	All Core module's
-	*	Services
-	*	Controllers
-	*	Directives
-	*	filters
-	*	Providers etc.
-	*	should be required here and defined in raiweb.core module
-	*/
+                $stateProviderRef.state(state);
+                createStates(navItem.navs, stateName);
+            });
+        }
 
-	angular.module('raiweb.core')
-	.factory('responsiveDetectionService', require('./services/responsiveDetectionService'))
-	.factory('appService', require('./services/appService'))
-	.factory('pageTitleService', require('./services/pageTitleService'))
-	.factory('metaInformationService', require('./services/metaInformationService'))
-	.factory('appHeaderService', require('./services/appHeaderService'))
-	.controller('appController', require('./controllers/appController'))
-	.directive('appHeader', require('./directives/appHeader'))
-	.directive('brandLogo', require('./directives/brandLogo'));
+        /*
+        *   setAppHeader is a private method
+        *   It takes an Object param, and sets the various Header Information
+        *   If param of anyother type is passed, it resets all the Header Information to null.
+        *   So This method can be used to reset Header Information by passing false valueto param
+        */
+        function setAppHeader(headerInfo) {
+            if (headerInfo instanceof Object) {
+                $rootScope.appHeader = {
+                    logo: headerInfo.logo || null,
+                    navs: headerInfo.navs || null
+                };
+            } else {
+                $rootScope.appHeader = {
+                    logo: null,
+                    navs: null
+                };
+            }
+        }
 
-	module.exports = angular.module('raiweb.core');
+        /*
+        *   setMetaInformation is a private method
+        *   It takes an Object param, and sets the following meta Information of Page:
+        *   keywords, description and page Title
+        */
+        function setMetaInformation(metaInfo) {
+            if (metaInfo instanceof Object) {
+                metaInformationService.reset();
+                metaInformationService.appendMetaKeywords(metaInfo.keywords);
+                metaInformationService.setMetaDescription(metaInfo.description);
+                pageTitleService.setPageTitle(metaInfo.title);
+            } else {
+                metaInformationService.reset();
+                pageTitleService.setPageTitle();
+            }
+        }
+    }]);
+
+    /*
+    *   Description
+    *   All Core module's
+    *   Services
+    *   Controllers
+    *   Directives
+    *   filters
+    *   Providers etc.
+    *   should be required here and defined in raiweb.core module
+    */
+
+    angular.module('raiweb.core')
+    .factory('responsiveDetectionService', require('./services/responsiveDetectionService'))
+    .factory('appService', require('./services/appService'))
+    .factory('pageTitleService', require('./services/pageTitleService'))
+    .factory('metaInformationService', require('./services/metaInformationService'))
+    .factory('appHeaderService', require('./services/appHeaderService'))
+    .controller('appController', require('./controllers/appController'))
+    .directive('appHeader', require('./directives/appHeader'))
+    .directive('brandLogo', require('./directives/brandLogo'));
+
+    module.exports = angular.module('raiweb.core');
 })();
