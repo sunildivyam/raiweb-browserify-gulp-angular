469ce40674eb3339419e94c0f6026cf62dea7ab4
Sunil Kumar <sunil.divyam@gmail.com>
2016-06-30 20:26:25 +0530

    RAIWEB | angular-animate module added

--- web/app/index.js
+++ web/app/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var angular = require('angular');
 require('angular-sanitize');
+require('angular-animate');
 
 window.$ = require('jquery');
 window.jQuery = window.$;
@@ -14,6 +15,7 @@ angular.module('raiweb', [
 	'ui.router',
 	'templates',
 	'ngSanitize',
+	'ngAnimate',
 	require('./js/core/core').name,
 	require('./js/home/home').name,
 	require('./js/services/services').name,
